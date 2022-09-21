d0a823be65ee0f2d02ef1354e4b91fcfa24eaf54
Newquay365 <99223364+hughes3141@users.noreply.github.com>
2022-09-21 23:23:56 +0100

    Chnages to upcoming_assignment embed. Now uses a function that takes group from groupid_array in database, allowing search for those students in multiple groups.

--- user/upcoming_assignment_embed1.0.php
+++ user/upcoming_assignment_embed1.0.php
@@ -28,11 +28,57 @@ $result = $stmt->get_result();
 
 if($result) {
   $row=$result->fetch_assoc();
-  $groupid = $row['groupid'];
+  $groupid = json_decode($row['groupid_array']);
   $name = $row['name'];
 }
 
 
+
+function getUpcomingAssignmentsArray($groupIdArray) {
+
+  /*
+  This function generates an array of upcoming assignmnents. Input is an array (in JSON form) of the groups that a studnet is listed in.
+  */
+
+  global $conn;
+  $t = time();
+  $now = $now = date("Y-m-d H:i:s", $t);
+
+  $groupIdSql = array();
+
+  $list = array();
+  $paramType = "";
+
+  $sql = "SELECT * FROM assignments WHERE (";
+
+  $count = count($groupIdArray);
+  for($x =0; $x< $count; $x++) {
+    $valueSql = '%\"'.$groupIdArray[$x].'\"%';
+    array_push($groupIdSql, $valueSql);
+    $sql .= "groupid_array LIKE ? ";
+    if($x < ($count - 1)) {
+      $sql .= " OR ";
+    }
+    $paramType .= "s";
+  }
+  
+  $sql .= ") AND dateDue > CURRENT_TIMESTAMP()";
+
+  //echo $sql."<br>".$paramType;
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param($paramType, ...$groupIdSql);
+  $stmt->execute();
+  $result = $stmt->get_result();
+
+  if($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+      array_push($list, $row);
+    }
+  }
+
+  return $list;
+
+}
 
 function getUpcomingAssignments($groupId) {
   global $conn;
@@ -61,7 +107,7 @@ function getUpcomingAssignments($groupId) {
 
 }
 
-$assignments = getUpcomingAssignments($groupid);
+$assignments = getUpcomingAssignmentsArray($groupid);
 
 //print_r($assignments);
 
