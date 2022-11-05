3949b03c1be898bf58cea682ddf53e05fb786c8e
Newquay365 <99223364+hughes3141@users.noreply.github.com>
2022-11-05 00:13:19 +0000

    Updating saq_exercisecreate2.0 to now update to saq_exercies table. Includes list of previously-created exercises (a very basic list that needed editing function) and ability to add components.

--- saq/saq_exercisecreate2.0.php
+++ saq/saq_exercisecreate2.0.php
@@ -27,6 +27,53 @@ else {
 
 print_r($_POST);  
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  if($_POST['submitComps'] =="Submit") {
+    $count = $_POST['count'];
+    $count = intval($count);
+
+    $componentsArray = array();
+    
+    
+    for($x=0; $x<$count; $x++) {
+      array_push($componentsArray, array($_POST['input_'.$x.'_0'], $_POST['input_'.$x.'_1']));
+
+    }
+
+    //print_r($componentsArray);
+    
+
+    $componentsArray = json_encode($componentsArray);
+    //echo $componentsArray;
+
+    $datetime = date("Y-m-d H:i:s");
+
+
+    $sql="INSERT INTO saq_exercises (exerciseName, topic, structure, notes, dateCreated, userCreate) VALUES (?, ?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+
+    $stmt->bind_param("sssssi", $_POST['quizName'], $_POST['topic'], $componentsArray, $_POST['notes'], $datetime, $_SESSION['userid']);
+
+
+    if($_POST['quizName'] != "") {
+      $stmt->execute();
+      
+      echo "New records created successfully";
+    }
+
+    else {
+      echo "Name of exercise needed";
+    }
+      
+      
+      
+    
+
+    
+    
+
+  }
+}
 
 ?>
 
@@ -49,6 +96,14 @@ print_r($_POST);
     <style>
       .controlDiv {
         border: 1px solid black;
+      }
+
+      td, th {
+        border: solid 1px black;
+        padding: 3px;
+      }
+      table {
+        border-collapse: collapse;
       }
     </style>
   </head>
@@ -73,52 +128,128 @@ print_r($_POST);
         <button type ="button" onclick="addComponent()">Add Component</button>
     </form>
 
+    <?php
+      if(!isset($_GET['limit'])) {
+        $limit = 10;
+      } else {
+        $limit = $_GET['limit'];
+      }
+    
+    ?>
+    
+    <form method="post">
+    <p>
+      <label for="quizName">SAQ Exercise Name</label>
+      <input type="text" id="quizName" name="quizName">
+
+      <label for="topic">Topic</label>
+      <input type="text" id="topic" name="topic">
+
+   
+      <label for="notes">Notes</label>
+      <input type="text" id="notes" name="notes">
+    </p>
     <div id="componentDiv">
     </div>
+    <input type="hidden" name="count" id="inputCount">
+    <input type="submit" name="submitComps" value="Submit">
+    </form>
+
+
+    <h2>Exercise List</h2>
+    <form method ="get" action="">
+      <label for = "limit_pick">Limit: </label>
+      <input type="number" id="limit_pick" min = "0" name="limit" value="<?=$limit?>">
+      <input type="submit" value="Change Limit">
+    </form>
+    <br>
+    <table>
+      <tr>
+        <th>id</th>
+        <th>Topic</th>
+        <th>Exercise Name</th>
+        <th>Structure</th>
+        <th>Notes</th>
+        <th>Date Created</th>
+        <th>Edit</th>
+      </tr>
+      <?php
+
+      $sql = "SELECT * FROM saq_exercises ORDER BY dateCreated desc LIMIT ?";
+
+
+      if(isset($_GET['limit'])) {
+        $_GET['limit']= intval($_GET['limit']);
+        //var_dump($_GET['limit']);
+
+          $limit = $_GET['limit'];
+        
+      }
+
+
+
+      $stmt=$conn->prepare($sql);
+      $stmt->bind_param("i", $limit);
+      $stmt->execute();
+      $result= $stmt->get_result();
+
+      if ($result->num_rows>0) {
+        
+        while ($row = $result->fetch_assoc()) {
+          
+          ?>
+            <tr>
+              <td><?=htmlspecialchars($row['id']);?></td>
+              <td><?=htmlspecialchars($row['topic']);?></td>
+              <td><?=htmlspecialchars($row['exerciseName']);?></td>
+              <td><?php
+              $structure = json_decode($row['structure']);
+                //print_r($structure);
+                foreach($structure as $value) {
+                  echo htmlspecialchars($value[0]).": ".htmlspecialchars($value[1])."<br>";
+                }
+                //htmlspecialchars($row['structure']);
+                  ?></td>
+              <td><?=htmlspecialchars($row['notes']);?></td>
+              <td><?=htmlspecialchars($row['dateCreated']);?></td>
+            </tr>
+
+
+        <?php
+        }
+
+      }
+
+
+      ?>
+    </table>
 
     
-    <form method="post" action="" style="display:none">
-          <p>
-            <label for="quizName">SAQ Exercise Name</label>
-            <input type="text" id="quizName" name="quizName">
-          </p>
-            <p>
-              <label for="c_type_1">Component 1 Type:</label>
-              <input type="text" id="c_type_1" name="c_type_1">
-              <label for="c_data_1">Component 1 Data:</label>
-              <input type="text" id="c_data_1" name="c_data_1">
-            </p>
-            <p>
-              <label for="c_type_2">Component 2 Type:</label>
-              <input type="text" id="c_type_2" name="c_type_2">
-              <label for="c_data_2">Component 2 Data:</label>
-              <input type="text" id="c_data_1" name="c_data_2">
-            </p>
-            <p>
-            <label for="c_type_3">Component 3 Type:</label>
-            <input type="text" id="c_type_3" name="c_type_3">
-            <label for="c_data_3">Component 3 Data:</label>
-            <input type="text" id="c_data_2" name="c_data_3">
-            </p>
-            <p>
-            <label for="notes">Notes</label>
-            <input type="text" id="notes" name="notes">
-          </p>
-          <p>
-            <input type="submit" name="submit" value="Create Exercise">
-          </p>
-    </form>
+    
     
     <script>
 
       index = 0;
+      var inputCount;
 
       function reOrder() {
         var componentDiv = document.getElementById("componentDiv");
         var subDivs = componentDiv.getElementsByTagName("div");
         for(var i=0; i<subDivs.length; i++) {
           subDivs[i].id="control_div_"+i;
+          var input = subDivs[i].getElementsByTagName("input");
+          for(var j=0; j<input.length; j++) {
+            input[j].name = "input_"+i+"_"+j;
+          }
         }
+
+        inputCount = i;
+        console.log(inputCount);
+
+        var inputCountInput = document.getElementById("inputCount");
+        inputCountInput.value = inputCount;
+
+        
         
 
       }
@@ -132,16 +263,23 @@ print_r($_POST);
         var typeLabel = document.createElement('p');
         typeLabel.innerHTML = input;
 
-        var input = document.createElement('input');
-        input.type = "text";
+        
+        var input2 = document.createElement('input');
+        //input2.type="hidden";
+        input2.value=input;
+        var input1 = document.createElement('input');
+        input1.type = "text";
+
+        
 
         var removeButton = document.createElement('button');
         removeButton.innerHTML = "Remove";
         removeButton.type = "button";
         removeButton.setAttribute("onclick", "removeButton(this.parentElement)");
 
-        div.appendChild(typeLabel);
-        div.appendChild(input);
+        //div.appendChild(typeLabel);
+        div.appendChild(input2);
+        div.appendChild(input1);
         div.appendChild(removeButton);
         
 
@@ -152,7 +290,7 @@ print_r($_POST);
 
       function addComponent() {
         var compType = document.getElementById("selectDrop").value;
-        console.log(compType);
+        //console.log(compType);
         var component = document.createElement(compType);
         component.innerHTML = compType;
         var parentDiv = document.getElementById("componentDiv");
@@ -167,7 +305,7 @@ print_r($_POST);
       
 
       function removeButton(input)  {
-        console.log(input);
+        //console.log(input);
         //var div = document.getElementById(input);
         //div.remove();
         input.remove();
