f33b4203ba7d25723f6694ee0771771153216d89
Newquay365 <99223364+hughes3141@users.noreply.github.com>
2022-11-11 20:41:40 +0000

    Updated and working versions of exercises.php and 1.6.6oligopoly_cases.php

--- exercises/1.6.6oligopoly_cases.php
+++ exercises/1.6.6oligopoly_cases.php
@@ -15,10 +15,16 @@ include($path."/php_functions.php");
 
 include($path."/header_tailwind.php"); ?>
 
+<!--
+  This page will automatically update with new articles with new additions to the news database with the keyword 'oligopoly case study'
+
+
+-->
+
 <div class="container mx-auto px-4 mt-20 lg:mt-32 xl:mt-20 lg:w-1/2">
   <h1 class="font-mono text-2xl bg-pink-400 pl-1">1.6.6 Oligopoly Case Studies</h1>
   <div class="container mx-auto px-0 mt-2 bg-white text-black">
-    <p>Use the case studies below to read information on cartels and oligopolies.</p>
+    <p class="ml-2 text-xl">Use the case studies below to read information on cartels and oligopolies.</p>
 
     <ul>
 
