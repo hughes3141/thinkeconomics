5a05f3702886569ebe06354c3bcd8ca5cd19cd42
hughes3141 <hughes.ryan@gmail.com>
2022-11-16 23:48:04 +0000

    A few changes to quick_quiz: formatting of questions and adding question numbers

--- revision/quick_quiz.php
+++ revision/quick_quiz.php
@@ -28,6 +28,13 @@ include ($path."/header_tailwind.php");
                 $_GET['topics'] = $_GET['topic'];
               }
 
+              $questionNosBool = 0;
+              if(isset($_GET['questionNos'])) {
+                $questionNosBool = 1;
+              }
+
+
+
               //Array of questions set by the teacher, filtered by topic:
 
               $questions = array();
@@ -109,19 +116,39 @@ include ($path."/header_tailwind.php");
 
 ?>
 
+<!--
+
+GET Variables:
+  topic
+  topics
+  number:number of questions =10
+  questionNos if set then question numbers displayed
+
+-->
+
 
 <div class="container mx-auto px-4 mt-20 lg:mt-32 xl:mt-20 lg:w-3/4">
 
   <h1 class="font-mono text-2xl bg-pink-400 pl-1 ">Quick Revision Quiz</h1>
   <div class="container mx-auto px-0 mt-2 bg-white text-black ">
-    <div class="grid md:grid-cols-2 gap-4">    <?php 
+    <div class="grid md:grid-cols-2 gap-4">    
+      <?php 
+      $questionNumber = 1;
       foreach($randomQuestions as $key=>$question) {
         ?>
         
         <div class="content-center">
-          <div class=" question text-center m-3 py-2 px-4 bg-pink-400 text-white font-semibold rounded-lg shadow-md hover:bg-sky-300 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75" onclick="showAnswers(<?=$key;?>)">
+          <div class=" question text-center m-3 py-2 px-4  text-black border-4 border-pink-400  rounded-lg shadow-md hover:border-sky-300 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75" onclick="showAnswers(<?=$key;?>)">
 
-            <?=$question['question'];?>
+          <!-- bg-pink-400 text-white font-semibold-->
+
+            <?php
+              if($questionNosBool == 1) {
+                echo $questionNumber.": ";
+              }
+                
+              echo $question['question'];
+              ?>
 
         </div>
           <div class="answer hidden    m-3 py-2 px-4 border-4 border-sky-300 rounded-lg whitespace-pre-line"><?//=$question['topic'];?><?=$question['model_answer'];?>
@@ -131,6 +158,8 @@ include ($path."/header_tailwind.php");
         </div>
 
         <?php
+
+        $questionNumber ++;
 
       }
     ?>
@@ -149,7 +178,7 @@ include ($path."/header_tailwind.php");
 
 <script>
 
-  console.log("$GET Variables: topic, topics, number:number of questions =10")
+
   function showAnswers(num) {
         var answers = document.getElementsByClassName("answer");
         for (var i=0; i<answers.length; i++) {
@@ -163,7 +192,7 @@ include ($path."/header_tailwind.php");
         for (var i=0; i<questions.length; i++) {
           if (i == num) {
             questions[i].classList.remove("bg-pink-400");
-            questions[i].classList.add("bg-sky-300");
+            questions[i].classList.add("border-sky-300");
           }
 
         }
