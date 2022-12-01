aa27860d8528be14303b2df7e455157190ea67f8
Newquay365 <99223364+hughes3141@users.noreply.github.com>
2022-12-01 22:36:46 +0000

    Quick change to revision.php to allow for block-style hyperlinks

--- revision.php
+++ revision.php
@@ -25,19 +25,19 @@ include "header_tailwind.php";
       <ul class="list-none">
         <h2 class="font-mono text-xl bg-pink-300 pl-1"></h2>
           <h3 class="font-mono text-lg bg-pink-200 pl-1">FlashCards Collections</h3>
-            <li class="ml-2 hover:bg-sky-100" ><a href="revision/flashcards.php">FlashCards (All Topics)</a></li>
-            <li class="ml-2 hover:bg-sky-100" ><a href="revision/flashcards.php?topics=1.1.1,1.1.2,1.1.3">FlashCards (1.1 Topics)</a></li>
-            <li class="ml-2 hover:bg-sky-100" ><a href="revision/flashcards.php?topics=1.2.1,1.2.2,1.2.3,1.2.4">FlashCards (1.2 Topics)</a></li>
-            <li class="ml-2 hover:bg-sky-100" ><a href="revision/flashcards.php?topics=1.3.1,1.3.2">FlashCards (1.3 Topics)</a></li>
-            <li class="ml-2 hover:bg-sky-100" ><a href="revision/flashcards.php?topics=1.7.1,1.7.2,1.7.3">FlashCards (1.7 Topics)</a></li>
-            <li class="ml-2 hover:bg-sky-100" ><a href="revision/flashcards.php?topics=2.1.1,2.1.2,2.1,3,2.1.4,2.1.5,2.1.6,2.1.7,2.1.8">FlashCards (2.1 Topics)</a></li>
+            <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/flashcards.php">FlashCards (All Topics)</a></li>
+            <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/flashcards.php?topics=1.1.1,1.1.2,1.1.3">FlashCards (1.1 Topics)</a></li>
+            <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/flashcards.php?topics=1.2.1,1.2.2,1.2.3,1.2.4">FlashCards (1.2 Topics)</a></li>
+            <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/flashcards.php?topics=1.3.1,1.3.2">FlashCards (1.3 Topics)</a></li>
+            <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/flashcards.php?topics=1.7.1,1.7.2,1.7.3">FlashCards (1.7 Topics)</a></li>
+            <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/flashcards.php?topics=2.1.1,2.1.2,2.1,3,2.1.4,2.1.5,2.1.6,2.1.7,2.1.8">FlashCards (2.1 Topics)</a></li>
           <h3 class="font-mono text-lg bg-pink-200 pl-1">Quick Quizzes</h3>
-            <li class="ml-2 hover:bg-sky-100" ><a href="revision/quick_quiz.php">Quick Quiz (All Topics)</a></li>
-            <li class="ml-2 hover:bg-sky-100" ><a href="revision/quick_quiz.php?topics=1.1.1,1.1.2,1.1.3">Quick Quiz (1.1 Topics)</a></li>
-            <li class="ml-2 hover:bg-sky-100" ><a href="revision/quick_quiz.php?topics=1.2.1,1.2.2,1.2.3,1.2.4">Quick Quiz (1.2 Topics)</a></li>
-            <li class="ml-2 hover:bg-sky-100" ><a href="revision/quick_quiz.php?topics=1.3.1,1.3.2">Quick Quiz (1.3 Topics)</a></li>
-            <li class="ml-2 hover:bg-sky-100" ><a href="revision/quick_quiz.php?topics=1.7.1,1.7.2,1.7.3">Quick Quiz (1.7 Topics)</a></li>
-            <li class="ml-2 hover:bg-sky-100" ><a href="revision/quick_quiz.php?topics=2.1.1,2.1.2,2.1,3,2.1.4,2.1.5,2.1.6,2.1.7,2.1.8">Quick Quiz (2.1 Topics)</a></li>
+            <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/quick_quiz.php">Quick Quiz (All Topics)</a></li>
+            <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/quick_quiz.php?topics=1.1.1,1.1.2,1.1.3">Quick Quiz (1.1 Topics)</a></li>
+            <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/quick_quiz.php?topics=1.2.1,1.2.2,1.2.3,1.2.4">Quick Quiz (1.2 Topics)</a></li>
+            <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/quick_quiz.php?topics=1.3.1,1.3.2">Quick Quiz (1.3 Topics)</a></li>
+            <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/quick_quiz.php?topics=1.7.1,1.7.2,1.7.3">Quick Quiz (1.7 Topics)</a></li>
+            <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/quick_quiz.php?topics=2.1.1,2.1.2,2.1,3,2.1.4,2.1.5,2.1.6,2.1.7,2.1.8">Quick Quiz (2.1 Topics)</a></li>
 
 
       </ul>
