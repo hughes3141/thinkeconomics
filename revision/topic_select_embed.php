b2a1c946f6bd397414b503f1fe73a2d20e570f06
Newquay365 <99223364+hughes3141@users.noreply.github.com>
2023-07-09 22:34:55 +0100

    Removed yet more bugs from topic_select_embed.php

--- revision/topic_select_embed.php
+++ revision/topic_select_embed.php
@@ -71,7 +71,7 @@
           
           <h2 id="accordion-collapse-heading-2">
             <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left  text-gray-500  border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-2" aria-expanded="<?php
-              if((!empty($subjectIdSet) && !empty($examBoardId)) && (is_null($topicIdsArray) or count($topicIdsArray)<=1)) {
+              if((!empty($subjectIdSet) && !empty($examBoardId)) && (empty($topicIdsArray) or (count($topicIdsArray)<=1 && $topicIdsArray[0] == ""))) {
                 echo "true";
               } else {
                 echo "false";
@@ -79,8 +79,7 @@
               ?>" aria-controls="accordion-collapse-body-2">
               <span>Select Topics</span>
               <?php
-
-
+                //var_dump($topicIdsArray);
               ?>
               <svg data-accordion-icon class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </button>
