110792e25520e6ee756db77fff06fd7fd1fb9433
Ryan Hughes <99223364+hughes3141@users.noreply.github.com>
2023-09-07 13:02:36 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -26,7 +26,7 @@ include($path."/header_tailwind.php");
     <ul class="list-none">
       <li class="ml-2 mr-2 hover:bg-sky-100"><a class = "block" href="admin/courseguide_21.php" >Course Guide</a></li>
       <li class="ml-2 mr-2 hover:bg-sky-100"><a class = "block" href="admin/course_intro.php">Pre-Course Information: What is Economics?</a></li>
-      <li class="ml-2 mr-2 hover:bg-sky-100"><a class = "block" href="admin/y1_yearplan_21.php">Year 1 Year Plan</a></li>
+      <li class="ml-2 mr-2 hover:bg-sky-100"><a class = "block" href="admin/y1_yearplan.php">Year 1 Year Plan</a></li>
       <li class="ml-2 mr-2 hover:bg-sky-100"><a class = "block" href="admin/y2_yearplan_21.php">Year 2 Year Plan</a></li>
       <li class="ml-2 mr-2 hover:bg-sky-100"><a class = "block" href="admin/topic_list.php">Eduqas A Level Topic list</a></li>
       <li class="ml-2 mr-2 hover:bg-sky-100"><a class = "block" href ="admin/eduqas-a-economics-spec-from-2015.pdf" target ="_blank">A Level Economics Specification</a></li>
