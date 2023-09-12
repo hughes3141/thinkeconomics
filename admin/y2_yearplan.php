64bdd29c7f26996ba4eeae502eacba197bab66c4
Ryan Hughes <99223364+hughes3141@users.noreply.github.com>
2023-09-12 11:01:13 +0100

    Update y2_yearplan.php
    Update to change Y2 in heading

--- admin/y2_yearplan.php
+++ admin/y2_yearplan.php
@@ -98,7 +98,7 @@ $holidays = (array) json_decode('{
 ?>
 
 <div class="container mx-auto px-4 mt-20 lg:mt-32 xl:mt-20 lg:w-3/4">
-  <h1 class="font-mono text-2xl bg-pink-400 pl-1">Year 1 A Level Economics Year Plan 2023-2024</h1>
+  <h1 class="font-mono text-2xl bg-pink-400 pl-1">Year 2 A Level Economics Year Plan 2023-2024</h1>
   <div class=" container mx-auto px-4 pb-4 mt-2 bg-white text-black mb-5 pt-4">
 
 
