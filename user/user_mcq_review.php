69064b79565ba5d2f74924ff0e45d62af8cd6d99
Ryan Hughes <99223364+hughes3141@users.noreply.github.com>
2023-10-04 16:09:28 +0100

    Update user_mcq_review.php

--- user/user_mcq_review.php
+++ user/user_mcq_review.php
@@ -69,7 +69,7 @@ include($path."/header_tailwind.php");
           ?>
         <div id="accordion-collapse" data-accordion="collapse" class="my-2 border rounded-xl border-gray-200 mt-2 mb-2">
           <h2 id="accordion-collapse-heading-1">
-            <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-1" aria-expanded="<?=(empty($responseId)) ? "true" : "false"?>" aria-controls="accordion-collapse-body-1">
+            <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 " data-accordion-target="#accordion-collapse-body-1" aria-expanded="<?=(empty($responseId)) ? "true" : "false"?>" aria-controls="accordion-collapse-body-1">
               <span>MCQ Record</span>
               <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
@@ -77,7 +77,7 @@ include($path."/header_tailwind.php");
             </button>
           </h2>
           <div id="accordion-collapse-body-1" class="hidden" aria-labelledby="accordion-collapse-heading-1">
-            <div class=" py-5  border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+            <div class=" py-5  border-b-0 border-gray-200 ">
               <table class="w-full table-fixed font-sans">
                 <tr class="font-mono sticky">
                     <th class="text-xs md:text-lg"><span class="hidden md:inline">MCQ </span>Exercise<span class="hidden md:inline"> Name</span></th>
