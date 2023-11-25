9b532654890ef22716ccd945903c5ced2aee5388
Newquay365 <99223364+hughes3141@users.noreply.github.com>
2023-11-25 22:01:22 +0000

    Took out all of legacy javascript and associated divs from mcq_exercise.php. Also sorted out a label issue on mcq_questions.php

--- mcq/mcq_exercise.php
+++ mcq/mcq_exercise.php
@@ -229,7 +229,7 @@ include($path."/header_tailwind.php");
 						}
 						?>
 						<p class="mb-2">
-							<input type="radio" id="a_<?=$question?>_<?=$optKey?>" name="a_<?=$question?>" value="<?=$optKey?>" onclick="questionRecord(<?=$question?>)">
+							<input type="radio" class="" id="a_<?=$question?>_<?=$optKey?>" name="a_<?=$question?>" value="<?=$optKey?>" onclick="questionRecord(<?=$question?>)">
 						<label class=" " for="a_<?=$question?>_<?=$optKey?>"><?=$option?></label>
 						</p>
 						<?php
@@ -246,47 +246,11 @@ include($path."/header_tailwind.php");
 	</form>
 
 
-    <form id="myForm" method="post" action="" style="display:none  ;">
-    <p>Name: <input type = "text" name ="name" id = "name" style="" value="<?=$userInfo['name_first']?> <?=$userInfo['name_last']?>"> </p>
-    <input type = "text" name ="record" id = "f1" style="display:;" >
-    <input type = "text" name ="quizname" id = "f2" style="display: ;" value = "<?=$quizInfo['quizName']?>">
-    <input type = "text" name ="startTime" value = "<?php echo date("Y-m-d H:i:s") ?>" style="display:;">
-    <input type = "text" name ="assignid" value ="<?= (isset($_GET['assignid'])) ? $_GET['assignid'] : ""?>" style="display: ;" >
-    <input type = "text" name ="userid" value ="<?=$userId?>" style="display: ;" >
-
-    <input type = "text" name ="reviewQ" id = "reviewInput" style="display: ;" >
-    <input type = "text" name ="multi" id = "multiInput" style="display: ;" >
-    <input type ="text" name ="submit_info" value ="submittedForm">
-
-    </form>
-
-<button onclick="submit()" style="display:none">Click to submit</button>
-<button onclick="view()" style ="display:none;">Click to see console</button>
 
 
-<div class="p-2">
-<h2>Question <span id="q1"></span>/<span id="q2"></span></h2>
-<p class="text-xs"><em id = "q4"></em></p>
-<div class="flex flex-row">
-  <input type="button" class="flex-1 px-1 text-sm bg-sky-100 hover:bg-pink-300 disabled:opacity-75 p-1" value ="Previous Question" id="previous2">
-  <input type="button" class="flex-1 px-1 text-sm bg-sky-100 hover:bg-pink-300 disabled:opacity-75 p-1" value ="Submit" id="submit2">
-  <input type="button" class="flex-1 px-1 text-sm bg-sky-100 hover:bg-pink-300 disabled:opacity-75 p-1" value ="Next Question" id="next2">
-</div>
 
-<img src="" class="lg:w-3/4 mx-auto mt-3" alt="Question" id ="question" >
 
-	<div id= "d1" class="">
 
-	</div>
-
-<br>
-<div class="flex flex-row">
-  <input type="button" class="flex-1 px-1 text-sm bg-sky-100 hover:bg-pink-300 disabled:opacity-75 p-1" value ="Previous Question" id="previous">
-  <input type="button" class="flex-1 px-1 text-sm bg-sky-100 hover:bg-pink-300 disabled:opacity-75 p-1" value ="Submit" id="submit">
-  <input type="button" class="flex-1 px-1 text-sm bg-sky-100 hover:bg-pink-300 disabled:opacity-75 p-1" value ="Next Question" id="next">
-</div>
-
-</div>
 
 </div>
 
@@ -343,16 +307,16 @@ function submit2() {
 
   const alertIncompleteDiv = document.getElementById("alertIncompleteDiv")
 
-  console.log(attemptedQuestions.length+" "+count);
+  //console.log(attemptedQuestions.length+" "+count);
 
   if(attemptedQuestions.length != count) {
     alertIncompleteDiv.classList.remove("hidden");
     const difference = count - attemptedQuestions.length;
     const incompleteQuestionCount = document.getElementById("incompleteQuestionCount");
     incompleteQuestionCount.innerHTML = difference;
-    console.log(difference);
+    //console.log(difference);
     const isAreQuestionCount = document.getElementById("isAreQuestionCount");
-    console.log(isAreQuestionCount);
+    //console.log(isAreQuestionCount);
     const questionCountPlural = document.getElementById("questionCountPlural");
     if(difference > 1) {
       isAreQuestionCount.innerHTML = "are";
@@ -382,8 +346,8 @@ function questionRecord(questionid) {
   if(attemptedQuestions.includes(questionid) == false) {
     attemptedQuestions.push(questionid)
   }
-  console.log(attemptedQuestions);
-  console.log(count);
+  //console.log(attemptedQuestions);
+  //console.log(count);
 }
 
 
@@ -395,340 +359,6 @@ function questionRecord(questionid) {
 
 
 
-//////////////////////////////////
-
-
-var quizName = "<?=$quizInfo['quizName']?>";
-var index = [<?=$quizInfo['questions']?>];
-
-var record = []
-var record2 = []
-
-
-
-var question_no = 0;
-
-var next_id = document.getElementById("next");
-next_id.onclick=function() {next_q()};
-
-var previous_id = document.getElementById("previous");
-previous_id.onclick=function() {previous_q()};
-var previous_id2 = document.getElementById("previous2");
-previous_id2.onclick=function() {previous_q()};
-var next_id2 = document.getElementById("next2");
-next_id2.onclick=function() {next_q()};
-
-
-var submit_id = document.getElementById("submit");
-submit_id.onclick=function() {submit()};
-
-var submit_id2 = document.getElementById("submit2");
-submit_id2.onclick=function() {submit()};
-
-
-
-
-function populate() {
-	
-
-	for(var i=0; i<index.length; i++) {
-		var record3 = [];	
-		record3[0] = (index[i]).toFixed(6);
-		record3[1] = "";
-		record2.push(record3);
-	
-    }
-
-  var div = document.getElementById("d1");
-
-	load_q();
-	
-document.getElementById("q2").innerHTML= index.length;
-//document.getElementById("q3").innerHTML= index.length;
-
-previous_id.disabled = true;
-previous_id2.disabled = true;
-	
-}
-
-populate();
-
-function next_q() {
-	if (question_no < index.length-1) {
-	
-		record_mark();
-		question_no ++;
-		
-		load_q();
-		console.log(record2);
-	}
-	
-	if (question_no == index.length-1) {
-	
-		next_id.disabled = true;
-		next_id2.disabled = true;
-	}
-	
-	if (question_no != 0) {
-	
-		previous_id.disabled = false;
-		previous_id2.disabled = false;
-	}
-}
-
-function previous_q() {
-
-	if (question_no > 0) {
-		record_mark();
-		question_no --;
-		load_q();
-
-		console.log(record2);
-	}
-	
-	
-	if (question_no == 0) {
-	
-		previous_id.disabled = true;
-		previous_id2.disabled = true;
-	}
-	
-	if (question_no != index.length-1) {
-	
-		next_id.disabled = false;
-		next_id2.disabled = false;
-	}
-	
-	
-}
-
-function load_q() {
-	
-	document.getElementById("q1").innerHTML=question_no+1;
-	document.getElementById("q4").innerHTML=(index[question_no]).toFixed(6);
-	
-	/*
-	var i;
-	for (i=0; i<ans.length; i++) {
-	ans[i].checked=false;
-	}
-	
-	
-	if (record[question_no][2] != "" || record[question_no][2] == "0") {
-		ans[record[question_no][2]].checked =true;
-	}
-	*/
-	var question = document.getElementById("question");
-	
-	var bigjpg = (index[question_no]).toFixed(6)+".JPG";
-	var smljpg = (index[question_no]).toFixed(6)+".jpg";
-	
-	if (typeof bigjpg != "undefined") {
-		
-		question.setAttribute("src", "question_img/"+bigjpg);
-		
-	}else if (typeof smljpg != "undefined") {
-		
-		question.setAttribute("src", "question_img/"+smljpg);
-	}
-	
-	/*
-	question.setAttribute("src", "question_img/"+(index[question_no][0]).toFixed(6)+".jpg");
-	
-	*/
-	var div = document.getElementById("d1");
-	div.innerHTML ="";
-	//var button = [[],[],[],[],[]];
-	//var label = [[],[],[],[],[]]
-	var abcde = ["A", "B", "C", "D", "E"];
-	
-	/*
-	!!! This is where you can update the number of MCQ options that are given to the user. Use examBoardOptions array.
-	*/
-	
-	var optionsNumber;
-	var examBoardOptions = [["10",5],["11", 5],["12",4], ["13",5], ["14",4] ]
-	
-	for (var i=0; i<examBoardOptions.length; i++) {
-		
-		if ((index[question_no]).toString().substring(0,2) == examBoardOptions[i][0] ) {
-			
-			optionsNumber = examBoardOptions[i][1]
-			
-		}
-		
-		
-	}
-	console.log((index[question_no]).toString().substring(0,2));
-	
-	for(var j=0; j<optionsNumber; j++) {
-		var button = document.createElement("input");
-		button.setAttribute("type", "radio");
-		button.setAttribute("id", "a_"+question_no+"_"+j);
-		button.setAttribute("name", "a_"+question_no);
-		button.setAttribute("class", "button");
-		var label = document.createElement("label");
-		label.setAttribute("for", "a_"+question_no+"_"+j)
-		label.innerHTML = abcde[j];
-		
-		if(record2[question_no][1]==abcde[j]) {
-			button.checked = true;
-		}
-		
-		div.appendChild(button);
-		div.appendChild(label);
-
-		var br = document.createElement("br");
-		div.appendChild(br);
-	}
-		/*
-		button[j] = document.createElement("input");
-		button[j].setAttribute("type", "radio");
-		button[j].setAttribute("id", "a_"+question_no+"_"+j);
-		button[j].setAttribute("class", "button");
-		button[j].setAttribute("name", "a_"+question_no);
-		label[j] = document.createElement("label");
-		label[j].innerHTML = abcde[j];
-		div.appendChild(label[j]);
-		div.appendChild(button[j]);
-		div.appendChild("<br")}
-		*/
-	console.log(question_no);
-	
-
-}
-
-function record_mark() {
-
-	var abcde = ["A", "B", "C", "D", "E"]
-	var answer ="";
-	var answer_code="";
-	var checked = false;
-	
-	//record2[question_no][0]=index[question_no][0];
-	
-	var buttons = document.getElementsByClassName("button");
-	for (var i=0; i<buttons.length; i++) {
-	
-	//var i;
-	//	for (i=0; i<ans.length; i++) {
-		if (buttons[i].checked == true) {
-			answer = abcde[i];
-			answer_code = i;
-		};
-				
-}
-		
-	record2[question_no][1]= answer;
-	//record[question_no][2]= answer_code;
-	
-
-	
-
-
-}
-
-function submit() {
-	
-	record_mark();
-
-	record = [];
-	
-	name = document.getElementById("name").value;
-	console.log(name);
-
-if (name == "") {alert("Please enter your name")}
-
-else {
-	
-	
-	var r = confirm("You are about to submit your answers. This will record your score.");
-			if (r == true)
-			
-			{
-	
-
-				/*for(var i=0; i<index.length; i++) {
-					var a = document.getElementById("a_"+i+"_0");
-					var b = document.getElementById("a_"+i+"_1");
-					var c = document.getElementById("a_"+i+"_2");
-					var d = document.getElementById("a_"+i+"_3");
-					var e = document.getElementById("a_"+i+"_4");
-					
-					var questionRecord = [];
-					questionRecord[0] = (index[i]).toFixed(6);
-					
-					if (a.checked == true) {
-						questionRecord[1] = "A"
-						}
-					else if (b.checked == true) {
-						questionRecord[1] = "B"
-						}
-						
-					else if (c.checked == true) {
-						questionRecord[1] = "C"
-						}
-					else if (d.checked == true) {
-						questionRecord[1] = "D"
-						}
-					else if (e.checked == true) {
-						questionRecord[1] = "E"
-						}
-					else {
-					questionRecord[1] = ""
-						}
-					
-					record.push(questionRecord);
-				}
-				*/
-				record = JSON.stringify(record2);
-				console.log(record);
-				document.getElementById("f1").value = record;
-				//document.getElementById("f2").value = quizName;
-				document.getElementById("myForm").submit();
-			}
-	}
-}
-
-function view() {
-	
-	record = [];
-
-	for(var i=0; i<index.length; i++) {
-		var a = document.getElementById("a_"+i+"_0");
-		var b = document.getElementById("a_"+i+"_1");
-		var c = document.getElementById("a_"+i+"_2");
-		var d = document.getElementById("a_"+i+"_3");
-		
-		var questionRecord = [];
-		questionRecord[0] = (index[i]).toFixed(6);
-		
-		if (a.checked == true) {
-			questionRecord[1] = "A"
-			}
-		else if (b.checked == true) {
-			questionRecord[1] = "B"
-			}
-			
-		else if (c.checked == true) {
-			questionRecord[1] = "C"
-			}
-		else if (d.checked == true) {
-			questionRecord[1] = "D"
-			}
-		else {
-		questionRecord[1] = ""
-			}
-		
-		record.push(questionRecord);
-	}
-	
-	record = JSON.stringify(record);
-	console.log(record);
-	document.getElementById("f1").value = record;
-	document.getElementById("f2").value = quizName;
-	
-}
 
 </script>
 
