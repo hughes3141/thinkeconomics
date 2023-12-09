7c71dfe7e5ab7436f2dd8096839ffe650f3b56d3
Newquay365 <99223364+hughes3141@users.noreply.github.com>
2023-12-09 05:36:09 +0000

    changes to user3.0 to allow for changed MCQ assets

--- user/user3.0.php
+++ user/user3.0.php
@@ -255,13 +255,13 @@ include "../header_tailwind.php";
                 <p class="ml-2 font-bold">Old resources</p>
                 <p class="ml-2 hover:bg-sky-100"><a class ="block" href="../mcq/mcq_list.php">List of MCQ Questions</a></p>
                 <p class="ml-2 hover:bg-sky-100"><a class ="block" href="../mcq/mcq_quizcreate.php">MCQ Quiz Creator and List</a></p>
-                <p class="ml-2 hover:bg-sky-100"><a class ="block" href="../mcq/mcq_student_review.php">MCQ Student Work Review</a></p>
+                <p class="ml-2 hover:bg-sky-100 hidden"><a class ="block" href="../mcq/mcq_student_review.php">MCQ Student Work Review</a></p>
                 
                 
                 
                 <p class="ml-2 font-bold">New resources</p>
-                <p class="ml-2 hover:bg-sky-100"><a class ="block" href="../mcq/mcq_questions.php">MCQ Question Manager</a></p><p class="ml-2 hover:bg-sky-100"><a class ="block" href="../mcq/quizcreate.php">MCQ Quiz Creator</a></p>
-                <p class="ml-2 hover:bg-sky-100"><a class ="block" href="../mcq/mcq_assignment_review3.0.php">MCQ Assignment Review</a></p>
+                <p class="ml-2 hover:bg-sky-100"><a class ="block" href="../mcq/mcq_questions.php">MCQ Question Manager</a></p>
+                <p class="ml-2 hover:bg-sky-100"><a class ="block" href="../mcq/quizcreate.php">MCQ Quiz Creator</a></p>
                 <p class="ml-2 hover:bg-sky-100"><a class ="block" href="../mcq/mcq_preview.php">MCQ Quiz Preview</a></p>
 
 
