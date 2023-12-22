0a406cfbac7640bf09a2afbdc1bdbaaf0fb5c747
Newquay365 <99223364+hughes3141@users.noreply.github.com>
2023-12-22 06:52:34 +0000

    Continuing work on email_try.php to enable emails from script

--- user/email_try.php
+++ user/email_try.php
@@ -1,6 +1,48 @@
 <?php
 
+$path = $_SERVER['DOCUMENT_ROOT'];
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require $path.'/PHPMailer-master/src/Exception.php';
+require $path.'/PHPMailer-master/src/PHPMailer.php';
+require $path.'/PHPMailer-master/src/SMTP.php';
+
+//echo $path;
+
 echo "This is trying email";
+
+$mail = new PHPMailer(true); // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail->SMTPDebug = 2; // Enable verbose debug output
+    $mail->isSMTP(); // Set mailer to use SMTP
+    $mail->Host = 'mail.thinkeconomics.co.uk'; // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true; // Enable SMTP authentication
+    
+    $mail->Username   = 'no_reply@thinkeconomics.co.uk';   // SMTP username
+    $mail->Password   = '';       // SMTP password
+
+    
+
+    $mail->SMTPSecure = 'tls'; // Enable SSL encryption, TLS also accepted with port 465
+    $mail->Port = 465; // TCP port to connect to
+    //Recipients
+    $mail->setFrom('no_reply@thinkeconomics.co.uk', 'ThinkEconomics Support'); //This is the email your form sends From
+
+    $mail->addAddress('hughes.ryan@gmail.com', 'Ryan Hughes'); // Add a recipient address
+    //Content
+    $mail->isHTML(true); // Set email format to HTML
+    $mail->Subject = 'Subject line goes here';
+    $mail->Body    = 'Body text goes here';
+    $mail->send();
+    echo 'Message has been sent';
+    
+} catch (Exception $e) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+}
+
 
 // the message
 $msg = "First line of text\nSecond line of text";
@@ -9,10 +51,10 @@ $msg = "First line of text\nSecond line of text";
 $msg = wordwrap($msg,70);
 
 // send email
-mail("hughes.ryan@gmail.com","My subject",$msg);
+//mail("hughes.ryan@gmail.com","My subject",$msg);
 
-echo $msg;
+//echo $msg;
 
-phpinfo();
+//phpinfo();
 
 ?>
