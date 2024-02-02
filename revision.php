1e780ba0206804ea41d03f3d8e78961129a74c42
Newquay365 <99223364+hughes3141@users.noreply.github.com>
2024-02-02 21:29:15 +0000

    I'm happy with this state of the revision page- knowledge organisers ordered by broad topic area

--- revision.php
+++ revision.php
@@ -25,19 +25,22 @@ include "header_tailwind.php";
       <ul class="list-none">
         <h2 class="font-mono text-xl bg-pink-300 pl-1"></h2>
           <h3 class="font-mono text-lg bg-pink-200 pl-1">Knowledge Organisers</h3>
-            <h4 class="pl-1 ml-1 mr-1 rounded my-1 text-lg bg-sky-100">Year 1 Micro</h4>
+            <h4 class="pl-1 ml-1 mr-1 rounded my-1 text-lg bg-sky-100">Microeconomics</h4>
               <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/knowledge_organiser.php?subjectLevel=1_1&examBoardId=1&topicIds=184%2C185%2C186">Knowledge Organiser (1.1 Topics)</a></li>
               <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/knowledge_organiser.php?subjectLevel=1_1&examBoardId=1&topicIds=187%2C188%2C189%2C190">Knowledge Organiser (1.2 Topics)</a></li>
               <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/knowledge_organiser.php?subjectLevel=1_1&examBoardId=1&topicIds=187%2C191%2C192">Knowledge Organiser (1.3 Topics)</a></li>
-              <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/knowledge_organiser.php?subjectLevel=1_1&examBoardId=1&topicIds=205%2C206%2C207">Knowledge Organiser (1.7 Topics)</a></li>
-            <h4 class="pl-1 ml-1 mr-1 rounded my-1 text-lg bg-sky-100">Year 1 Macro</h4>
-              <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/knowledge_organiser.php?subjectLevel=1_1&examBoardId=1&topicIds=208%2C209%2C210%2C211%2C212%2C215">Knowledge Organiser (2.1 Topics)</a></li>
-              <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/knowledge_organiser.php?subjectLevel=1_1&examBoardId=1&topicIds=218%2C219%2C220%2C221%2C222">Knowledge Organiser (2.2 Topics)</a></li>
-              <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/knowledge_organiser.php??subjectLevel=1_1&examBoardId=1&topicIds=223%2C224%2C226%2C227">Knowledge Organiser (2.3 Topics)</a></li>
-            <h4 class="pl-1 ml-1 mr-1 rounded my-1 text-lg bg-sky-100">Year 2 Micro</h4>
               <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/knowledge_organiser.php?subjectLevel=1_1&examBoardId=1&topicIds=193">Knowledge Organiser (1.4 Topics)</a></li>
               <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/knowledge_organiser.php?subjectLevel=1_1&examBoardId=1&topicIds=194%2C195%2C196">Knowledge Organiser (1.5 Topics)</a></li>
               <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/knowledge_organiser.php?subjectLevel=1_1&examBoardId=1&topicIds=197%2C198%2C199%2C200%2C201%2C202%2C203%2C204">Knowledge Organiser (1.6 Topics)</a></li>
+              <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/knowledge_organiser.php?subjectLevel=1_1&examBoardId=1&topicIds=205%2C206%2C207">Knowledge Organiser (1.7 Topics)</a></li>
+
+            <h4 class="pl-1 ml-1 mr-1 rounded my-1 text-lg bg-sky-100">Macroeconomics</h4>
+              <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/knowledge_organiser.php?subjectLevel=1_1&examBoardId=1&topicIds=208%2C209%2C210%2C211%2C212%2C215">Knowledge Organiser (2.1 Topics)</a></li>
+              <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/knowledge_organiser.php?subjectLevel=1_1&examBoardId=1&topicIds=218%2C219%2C220%2C221%2C222">Knowledge Organiser (2.2 Topics)</a></li>
+              <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/knowledge_organiser.php?subjectLevel=1_1&examBoardId=1&topicIds=223%2C224%2C226%2C227">Knowledge Organiser (2.3 Topics)</a></li>
+            <h4 class="pl-1 ml-1 mr-1 rounded my-1 text-lg bg-sky-100">Global Economcs</h4>
+              <li class="ml-2 hover:bg-sky-100" ><a class="block" href="revision/knowledge_organiser.php?subjectLevel=1_1&examBoardId=1&topicIds=228%2C229%2C230%2C231%2C232">Knowledge Organiser (3.1, 3.2, and 3.3 Topics)</a></li>
+           
              
         <?php
             /*
