53740aa7e7743179441f5e74b85139f180e9df50
Newquay365 <99223364+hughes3141@users.noreply.github.com>
2024-02-14 21:17:07 +0000

    Sorted out SVG to point in right direction

--- news/news_list.php
+++ news/news_list.php
@@ -117,7 +117,7 @@ GET variables:
           <button type="button" class="flex items-center justify-between w-full p-2 font-medium text-gray-500 border border-gray-200 hover:bg-gray-100  gap-3s font-mono" data-accordion-target="#accordion-collapse-body-1" aria-expanded="<?=($showSearch) ? "true" : "false"?>" aria-controls="accordion-collapse-body-1">
             <span>Search Controls</span>
             <svg data-accordion-icon class="w-3 h-3  shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1 5 5 1 1"/>
             </svg>
           </button>
         </h2>
