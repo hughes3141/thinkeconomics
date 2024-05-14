8e2e650092cec73f31b9111e9ab33b3a96380d7d
Newquay365 <99223364+hughes3141@users.noreply.github.com>
2024-05-14 23:17:13 +0100

    A working version of printfriendly.php, along with link in user3.0

--- news/printfriendly.php
+++ news/printfriendly.php
@@ -44,13 +44,29 @@ $style_input = "";
       textarea {
         width: 90%;
       }
+
+      @media print
+      {    
+          .no-print, .no-print *
+          {
+              display: none !important;
+          }
+      }
+
+      img {
+        float: right;
+        width: 50%;
+      }
     </style>
   </head>
   <body>
-    <h1>Print Friendly</h1>
-    <p>
-      <textarea id="input1" onchange="populateDocument();"></textarea>
+    <div class="no-print">
+      <h1 class="no-print">Print Friendly</h1>
+      <p >
+        <textarea id="input1" onchange="populateDocument();"></textarea>
+        <textarea id="input2" onchange="populateDocument();"></textarea>
     </p>
+    </div>
 
     <div id="generated" style="white-space: pre-line">
 
@@ -59,22 +75,52 @@ $style_input = "";
 
 	<script>
     var input1 =document.getElementById('input1');
+    var input2 =document.getElementById('input2');
     var generated =document.getElementById('generated');
 
     function populateDocument() {
       generated.innerHTML="";
       //console.log(input1.value);
       var text = input1.value;
+      var photos = input2.value
       var textArray = text.split("\n");
-      console.log(textArray);
-      //generated.innerHTML = input1.value;
+      var photoArray = photos.split(",");
 
       for(var x=0; x<textArray.length; x++) {
+        if(textArray[x] == "") {
+          textArray.splice(x,1);
+          x--;
+        }
+      }
+
+      
+      console.log(textArray);
+      console.log(photoArray);
+      //generated.innerHTML = input1.value;
+
+      for(var x=0; x<photoArray.length; x++) {
+        var img = document.createElement("img");
+        img.src = photoArray[x];
+        generated.appendChild(img);
+      }
+
+      for(var x=0; x<textArray.length; x++) {
+        if(x == 0) {
+
+          var p = document.createElement("h1");
+          const node = document.createTextNode(textArray[x]);
+          p.appendChild(node);
+        } else {
         var p = document.createElement("p");
         const node = document.createTextNode(textArray[x]);
         p.appendChild(node);
+        }
+
         generated.appendChild(p);
+        
       }
+
+
 
     }
   </script>
