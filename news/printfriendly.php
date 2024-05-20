b1d7642d6c232209151c844f885815aa7e151723
Newquay365 <99223364+hughes3141@users.noreply.github.com>
2024-05-20 16:03:33 +0100

    New version of printfriendly to allow for image control

--- news/printfriendly.php
+++ news/printfriendly.php
@@ -39,7 +39,7 @@ $style_input = "";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Print Friendly</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="">
     <style>
       textarea {
         width: 90%;
@@ -63,8 +63,15 @@ $style_input = "";
     <div class="no-print">
       <h1 class="no-print">Print Friendly</h1>
       <p >
+        <p><a href="/">Home</a></p>
+        <label>Aritle Text:</label><br>
         <textarea id="input1" onchange="populateDocument();"></textarea>
+        <br><label>Images</label><br>
         <textarea id="input2" onchange="populateDocument();"></textarea>
+        <label>Headings: </label>
+        <input id="input3" style="width:90%" type="text" onchange="populateDocument();">
+        <br><label>Img Order: </label>
+        <input id="input4" style="width:90%" type="text" onchange="populateDocument();">
     </p>
     </div>
 
@@ -76,9 +83,13 @@ $style_input = "";
 	<script>
     var input1 =document.getElementById('input1');
     var input2 =document.getElementById('input2');
+    var input3 =document.getElementById('input3');
     var generated =document.getElementById('generated');
 
     function populateDocument() {
+
+      console.clear();
+
       generated.innerHTML="";
       //console.log(input1.value);
       var text = input1.value;
@@ -86,37 +97,85 @@ $style_input = "";
       var textArray = text.split("\n");
       var photoArray = photos.split(",");
 
+      var headingsText = input3.value;
+
+      var imageOrder = input4.value;
+      var imageOrderArray = imageOrder.split(",");
+
+      for(var x=0; x<imageOrderArray.length; x++) {
+        imageOrderArray[x] = parseInt(imageOrderArray[x]);
+      }
+
+      var headings = headingsText.split(",");
+
       for(var x=0; x<textArray.length; x++) {
         if(textArray[x] == "") {
           textArray.splice(x,1);
           x--;
         }
       }
+      if(headingsText== "") {
+        for(var x=0; x<textArray.length; x++) {
+          var heading = "";
+          if(x == 0) {
+            heading = 'h1';
+          }
+          else {
+            heading = 'p';
+          }
+          
+          headings[x]=heading;
+        }
+      }
 
       
-      console.log(textArray);
+
+      if(imageOrder == "") {
+        for(var x=0; x<photoArray.length; x++) {
+          imageOrderArray[x] = parseInt(x);
+          console.log(imageOrderArray[x]);
+        }
+      }
+
+      input3.value = headings.join();
+      input4.value = imageOrderArray.join();
+
+      
+      console.log(headings);
+
+
+      //console.log(textArray);
       console.log(photoArray);
+      console.log(imageOrderArray)
       //generated.innerHTML = input1.value;
+      /*
 
       for(var x=0; x<photoArray.length; x++) {
         var img = document.createElement("img");
         img.src = photoArray[x];
         generated.appendChild(img);
       }
+      */
 
       for(var x=0; x<textArray.length; x++) {
-        if(x == 0) {
 
-          var p = document.createElement("h1");
-          const node = document.createTextNode(textArray[x]);
-          p.appendChild(node);
-        } else {
-        var p = document.createElement("p");
+        var p = document.createElement(headings[x]);
         const node = document.createTextNode(textArray[x]);
         p.appendChild(node);
-        }
-
         generated.appendChild(p);
+
+        if(imageOrderArray.includes(x)) {
+          //console.log("text"+x)
+          for(var y=0; y<imageOrderArray.length; y++) {
+            if(imageOrderArray[y]==x) {
+              var img = document.createElement("img");
+              img.src = photoArray[y];
+              generated.appendChild(img);
+
+            }
+          }
+        }
+        
         
       }
 
