83c4f8100f246f1aa262e7169f515c974bfd4654
Newquay365 <99223364+hughes3141@users.noreply.github.com>
2024-05-31 06:04:10 +0100

    More work on assignswitch.php, now with get selectors for studentId and groupId

--- user/assignswitch.php
+++ user/assignswitch.php
@@ -54,19 +54,24 @@ $changedResponses = array();
 
 if($_SERVER['REQUEST_METHOD']=='POST') {
   if($_POST['submit'] == "Assign Responses") {
-    $changedResponses = explode(",",$_POST['ids']);
-    foreach($changedResponses as $response) {
-      $message = updateMCQquizResults($response, $_POST['assignId_'.$response]);
-      array_push($changedResponses, $message);
+    if($_POST['ids'] != "") {
+      $changedResponses = explode(",",$_POST['ids']);
+      foreach($changedResponses as $response) {
+        //$message = updateMCQquizResults($response, $_POST['assignId_'.$response]);
+        //array_push($changedResponses, $message);
 
+      }
     }
   }
 
 }
 
 $get_selectors = array(
-  'articleId' => (isset($_GET['articleId']) && $_GET['articleId'] != "") ? $_GET['articleId'] : null,
-  'questionIds' => (isset($_GET['questionIds']) && $_GET['questionIds'] != "") ? $_GET['questionIds'] : null,
+  'groupId' => (isset($_GET['groupId']) && $_GET['groupId'] != "") ? $_GET['groupId'] : null,
+
+  'studentId' => (isset($_GET['studentId']) && $_GET['studentId'] != "") ? $_GET['studentId'] : null,
+
+
   'topic' => (isset($_GET['topic']) && $_GET['topic'] != "") ? $_GET['topic'] : null,
   'keyword' => (isset($_GET['keyword']) && $_GET['keyword'] != "") ? $_GET['keyword'] : null,
   'startDate' => (isset($_GET['startDate']) && $_GET['startDate'] != "") ? $_GET['startDate'] : null,
@@ -82,6 +87,35 @@ $get_selectors = array(
 
 );
 
+$students = getUsercreateUsers($userId);
+
+$studentId = null;
+
+if(isset($_GET['studentId'])) {
+  $studentId = $_GET['studentId'];
+}
+
+
+$studentInfo = getUserInfo($studentId);
+$student = $studentInfo;
+
+$groupid_array = array();
+if(!is_null($get_selectors['studentId'])) {
+  if($studentInfo['groupid_array'] != "") {
+    $groupid_array = json_decode($studentInfo['groupid_array']);
+  } 
+}
+
+$startDate = "20200901";
+
+$groupid_array_compare = array($get_selectors['groupId']);
+
+
+
+$assignments = getAssignmentsArray($groupid_array, $startDate, 1);
+
+
+
 include($path."/header_tailwind.php");
 
 ?>
@@ -90,59 +124,65 @@ include($path."/header_tailwind.php");
   <h1 class="font-mono text-2xl bg-pink-400 pl-1 mb-2">Assignment Switch</h1>
   <div class=" container mx-auto p-4 mt-2 bg-white text-black mb-5">
     <?php
+    if(!is_null($get_selectors['studentId'])){
+      ?>
+      <p>Student: <?=$studentInfo['name_first']?> <?=$studentInfo['name_last']?>
+      <?php
+    }
       print_r($_POST);
       print_r($changedResponses);
+      //print_r($studentInfo)
+      //print_r($students);
 
     ?>
+    <form method="get" action="">
+      <p>
+        <select name="studentId">
+          <option></option>
+          <?php
+          foreach($students as $row) {
+            ?>
+            <option value="<?=$row['id']?>" <?=$get_selectors['studentId'] == $row['id'] ? 'selected' : ''?>><?=$row['name_first']?> <?=$row['name_last']?></option>
+            <?php
+          }
+          ?>
+        </select>
+      </p>
+        <?php
+        if(isset($_GET['studentId'])) {
+          $groups = getGroupsList($userId);
+          //print_r($groups);
+        ?>
+          <p>
+            <select name="groupId">
+              <option></option>
+              <?php
+              foreach($groups as $row) {
+                ?>
+                <option value="<?=$row['id']?>" <?=$get_selectors['groupId'] == $row['id'] ? 'selected' : ''?>><?=$row['name']?></option>
+                <?php
+              }
+              ?>
 
-  <?php
+            </select>
 
-    $studentId = 213;
-    //E
+          </p>
+        <?php
+        }
+      ?>
 
-    //$studentId = 224;
-    //R
-    $userInfo = getUserInfo($studentId);
-    $student = $userInfo;
 
-    $switchedGroup = 35;
+      <button class="border border-black p-1 bg-pink-300 rounded">Select Student</button>
 
-    //print_r($userInfo);
-
-    echo "<br>";
-    echo $userInfo['name_first']." ".$userInfo['name_last'];
-
-    $groupid_array = array();
-    if($userInfo['groupid_array'] != "") {
-      $groupid_array = json_decode($userInfo['groupid_array']);
-    }
-
-    $groupid_array_compare = array($switchedGroup);
-
-    print_r($groupid_array_compare);
-    $startDate = "20200901"; 
-
-    $assignments = getAssignmentsArray($groupid_array, $startDate, 1);
-    $assignmentsCompare = getAssignmentsArray($groupid_array_compare, $startDate, 1);
-
-    //print_r($assignments);
-    foreach($assignments as $assignment) {
-      //echo "assignName: ".$assignment['assignName']." quizid: ".$assignment['quizid']." groupid: ".$assignment['groupid']." dateCreated: ".$assignment['dateCreated']." dateDue: ".$assignment['dateDue']."<br>";
-    }
-
-    echo "<br>";
-
-    foreach($assignmentsCompare as $assignment) {
-      //echo "assignName: ".$assignment['assignName']." quizid: ".$assignment['quizid']." groupid: ".$assignment['groupid']." dateCreated: ".$assignment['dateCreated']." dateDue: ".$assignment['dateDue']."<br>";
-    }
-
-    ?>
+    </form>
     <form method="post" action="">
       <!--
       <input name="count">
       -->
       <input name="ids" id="ids">
-      <input type="submit" name="submit" class="border p-1 bg-pink-300 rounded" value="Assign Responses"></button>
+      <input type="checkbox" id="selectAllCheckBox" onchange="checkAllBoxes();">
+      <label for="selectAllCheckBox">Select All</label>
+      <input id="inputFormSubmit" type="submit" name="submit" class="border p-1 bg-pink-300 rounded" value="Assign Responses" disabled>
       <table>
         <tr>
           <th>Current Group Assignments</th>
@@ -162,24 +202,26 @@ include($path."/header_tailwind.php");
               <td>
                 <?php
                   if($assignment['type']=='mcq') {
-                    $responses = getMCQquizResults2($student['id'], null, $assignment['quizid']);
+                    $responses = getMCQquizResults2($studentInfo['id'], null, $assignment['quizid']);
                     //print_r($responses);
                     echo "Student Responses: ";
                     echo count($responses)."<br>";
                     foreach($responses as $response) {
-                      echo "id: ".$response['id']." dateTime: ".$response['datetime']." duration: ".$response['duration']." percent: ".$response['percentage']." assignId: ".$response['assignId']." <br>assignName: ".$response['assignName']."<br>";
-                      ?>
-                      
-                      <!--
-                      <input type="number" name="responseId_<?=$response['id']?>" value="<?=$response['id']?>">
-                      -->
-                      <input type="number" name="assignId_<?=$response['id']?>" value="<?=$assignment['id']?>">
-                      <br>
-                      
-                      <input type="checkbox" name="" id="activeInput_<?=$response['id']?>" value="<?=$response['id']?>" class="activeInput" onchange="updateIdInput();">
+                      if($response['assignId'] != $assignment['id']) {
+                        echo "id: ".$response['id']." dateTime: ".$response['datetime']." duration: ".$response['duration']." percent: ".$response['percentage']." assignId: ".$response['assignId']." <br>assignName: ".$response['assignName']."<br>";
+                        ?>
+                        
+                        <!--
+                        <input type="number" name="responseId_<?=$response['id']?>" value="<?=$response['id']?>">
+                        -->
+                        <input type="number" name="assignId_<?=$response['id']?>" value="<?=$assignment['id']?>">
+                        <br>
+                        
+                        <input type="checkbox" name="" id="activeInput_<?=$response['id']?>" value="<?=$response['id']?>" class="activeInput" onchange="updateIdInput();">
 
 
-                      <?php
+                        <?php
+                      }
                     }
                   }
                 ?>
@@ -197,6 +239,7 @@ include($path."/header_tailwind.php");
       <div class="p-1">
         <h2>Student Version in Current Group:</h2>
         <?php
+
           include("user_assignment_list_embed.php");
           ?>
       </div>
@@ -236,6 +279,21 @@ include($path."/header_tailwind.php");
 
     ids.value = activeInputIds.join();
   }
+
+  function checkAllBoxes() {
+    const selectAllCheckBox = document.getElementById("selectAllCheckBox");
+    const activeInput = document.getElementsByClassName("activeInput");
+    for (var x=0; x<activeInput.length; x++) {
+      if(selectAllCheckBox.checked == true) {
+        activeInput[x].checked = true;
+      } else if (selectAllCheckBox.checked == false) {
+        activeInput[x].checked = false;
+      }
+    }
+    updateIdInput();
+  }
+
+  document.getElementById("inputFormSubmit").disabled=false;
 
 </script>
 
